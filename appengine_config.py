7f0d78f6e35c7416365a03471c75caa19228fca5
Gabor Feher <feherga@gmail.com>
2020-03-09 19:59:01 +0100

    python3

--- appengine_config.py
+++ appengine_config.py
@@ -1,18 +1,1 @@
 
-import pkg_resources
-from google.appengine.ext import vendor
-
-# Add any libraries installed in the "lib" folder.
-vendor.add('lib')
-
-import six
-reload(six)
-reload(pkg_resources)
-
-
-# Add libraries to pkg_resources working set to find the distribution.
-pkg_resources.working_set.add_entry('lib')
-
-pkg_resources.get_distribution('google-api-core')
-import google.cloud.ndb
-
