54940d44fa49cd9f2e88005f94a38887f5f0f309
Gabor Feher <feherga@gmail.com>
2020-04-23 21:56:46 +0200

    simplify the fetch offset repro

--- README.md
+++ README.md
@@ -1,35 +1,19 @@
 ```
 gcloud app deploy
 HOST=https://<your project>.appspot.com
-curl $HOST/cleanup  # optional
-curl $HOST/init/0
-# if crashes, check the logs and restart from the last saved
-# batch ID, e.g. if you see "saved until 3300", then run:
-curl $HOST/init/3300
-
-curl $HOST/offset/1000/10
-curl $HOST/offset/2000/10
-curl $HOST/offset/3000/10
+curl $HOST/demo
 ```
 
 Expected output:
 ```
-gabor@axl:~/ndb-testbed$ curl https://2-dot-myproject.appspot.com/offset/1000/10
-[1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1009, 1010]
-gabor@axl:~/ndb-testbed$ curl https://2-dot-myproject.appspot.com/offset/2000/10
-[2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010]
-gabor@axl:~/ndb-testbed$ curl https://2-dot-myproject.appspot.com/offset/3000/10
-[3001, 3002, 3003, 3004, 3005, 3006, 3007, 3008, 3009, 3010]
+gabor@axl:~/ndb-testbed$ curl https://2-dot-incidecoder2.appspot.com/demo
+[(995, 996), (996, 997), (997, 998), (998, 999), (999, 1000), (1000, 1001), (1001, 1001), (1002, 1002), (1003, 1003), (1004, 1004), (1005, 1005)]
 ```
 
 Observed output:
 ```
-gabor@axl:~/ndb-testbed$ curl https://2-dot-myproject.appspot.com/offset/1000/10
-[1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1009, 1010]
-gabor@axl:~/ndb-testbed$ curl https://2-dot-myproject.appspot.com/offset/2000/10
-[1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1009, 1010]
-gabor@axl:~/ndb-testbed$ curl https://2-dot-myproject.appspot.com/offset/3000/10
-[1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1009, 1010]
+gabor@axl:~/ndb-testbed$ curl https://2-dot-incidecoder2.appspot.com/demo
+[(995, 996), (996, 997), (997, 998), (998, 999), (999, 1000), (1000, 1001), (1001, 1001), (1002, 1001), (1003, 1001), (1004, 1001), (1005, 1001)]
 ```
 
 
--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from flask import Flask
 from google.cloud import ndb
 import logging
-import json
-import os
 import sys
 
 
@@ -24,91 +22,26 @@ def ndb_wsgi_middleware(wsgi_app):
     def middleware(environ, start_response):
         with ndb_client.context(global_cache=None) as context:
             context.set_cache_policy(False)
-            # The issue can also be reproduced with caching, but it's a bit
-            # simpler and maybe faster this way.
             return wsgi_app(environ, start_response)
     return middleware
 app.wsgi_app = ndb_wsgi_middleware(app.wsgi_app)
 
 
-class ProductCopy5(ndb.Model):
-    s = ndb.StringProperty()
-    b1 = ndb.BooleanProperty()
-    b2 = ndb.BooleanProperty()
-    b3 = ndb.BooleanProperty()
-    b4 = ndb.BooleanProperty()
-    i = ndb.IntegerProperty(repeated=True)
+@app.route('/demo')
+def demo():
+    class EmptyStuff(ndb.Model):
+        pass
 
-    @classmethod
-    def query_for_test(cls):
-        return cls.query(
-            cls.b1 == True,
-            cls.b2 == False,
-            cls.b3 == True,
-            cls.b4 == False,
-            cls.i == 1,
-            ancestor=cls.parent_key())
+    ndb.put_multi(
+        EmptyStuff(key=ndb.Key('EmptyStuff', i+1))
+        for i in range(1100))
 
-    @staticmethod
-    def parent_key(name = 'default'):
-        return ndb.Key('ProductCopy5', name)
-
-
-@app.route('/test')
-def test5():
-    def keys(prods):
-        return [p.key for p in prods]
-    def ids(keys):
-        return [k.id() for k in keys]
-    logging.getLogger('google.cloud.ndb').setLevel(logging.DEBUG)
-    ko = ProductCopy5.query_for_test().fetch(keys_only=True)
-    pro = ProductCopy5.query_for_test().fetch(projection=[ProductCopy5.s])
-    logging.getLogger('google.cloud.ndb').setLevel(logging.WARNING)
-    return f'''The following two lists should contain the same ids:<br>
-{ids(ko)} == {ids(keys(pro))}'''
-
-
-@app.route('/init/<int:offset>')
-def init(offset):
-    path = os.path.dirname(os.path.abspath(__file__)) + '/export.json'
-    with open(path, 'r') as f:
-        data = json.load(f)
-    k = offset + 1
-    to_put = []
-    for entry in data[offset:]:
-        pc5 = ProductCopy5(
-            key=ndb.Key('ProductCopy5', 'default', 'ProductCopy5', k),
-            s='hello',
-            b1=entry['b1'],
-            b2=entry['b2'],
-            b3=entry['b3'],
-            b4=entry['b4'],
-            i=entry['i'])
-        to_put.append(pc5)
-        k += 1
-        if len(to_put) >= 100:
-            ndb.put_multi(to_put)
-            logging.info(f'saved until {to_put[-1].key.id()}')
-            to_put = []
-    if len(to_put) > 0:
-        ndb.put_multi(to_put)
-        logging.info(f'saved until {to_put[-1].key.id()}')
-
-    return 'ok'
-
-
-@app.route('/offset/<int:offset>/<int:limit>')
-def offsetget(offset, limit):
-    q = ProductCopy5.query()
-    l = q.fetch(limit=limit, offset=offset)
-    return str([p.key.id() for p in l]) + '\n'
-
-
-@app.route('/cleanup')
-def cleanup():
-    ks = ProductCopy5.query().fetch(keys_only=True)
-    ndb.delete_multi(ks)
-    return 'ok'
+    q = EmptyStuff.query()
+    findings = [
+        (i, q.fetch(limit=1, offset=i)[0].key.id())
+        for i in range(995, 1006)
+    ]
+    return str(findings) + '\n'
 
 
 @app.route('/')
